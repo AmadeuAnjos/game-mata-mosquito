285a9b130cccb7bf326323ed4c1d71b3297a7f4b
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-19 21:32:15 -0300

    :tada: Commit inicial

---
+++ jogo.js
@@ -0,0 +1,17 @@
+
+var alt = 0
+var larg = 0
+
+function ajustaTela()  {
+    alt = window.innerHeight
+    larg = window.innerWidth
+     
+    //console.log(alt, larg)
+}
+
+ajustaTela()
+
+var posicaox = Math.random()
+var posicaoy = Math.random()
+
+console.log(posicaox, posicaoy)

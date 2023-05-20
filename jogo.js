223d331152dbcd2ece2d348c3f487b5bc37e10db
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-19 22:16:36 -0300

    :dizzy: inserindo animação nos msoquitos

--- jogo.js
+++ jogo.js
@@ -6,12 +6,26 @@ function ajustaTela()  {
     alt = window.innerHeight
     larg = window.innerWidth
      
-    //console.log(alt, larg)
+    console.log(alt, larg)
 }
 
 ajustaTela()
 
-var posicaox = Math.random()
-var posicaoy = Math.random()
+function positionRandom() {
+var posicaox = Math.floor(Math.random() * larg) - 90
+var posicaoy = Math.floor(Math.random() * alt) - 90
+
+posicaox = posicaox < 0 ? 0 : posicaox
+posicaoy = posicaoy < 0 ? 0 : posicaoy
 
 console.log(posicaox, posicaoy)
+
+var mosquito = document.createElement('img')
+mosquito.src = 'assets/imagens/mosca.png'
+mosquito.className='mosquito'
+mosquito.style.left = posicaox + 'px'
+mosquito.style.top = posicaoy + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+}

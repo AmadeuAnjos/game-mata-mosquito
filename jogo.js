e5b571f6cde8c886dbfbff4c12f08fa4906d81bd
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-23 21:13:09 -0300

    identando o código

--- jogo.js
+++ jogo.js
@@ -1,59 +1,57 @@
+var alt = 0;
+var larg = 0;
 
-    var alt = 0
-    var larg = 0
+function ajustaTela() {
+  alt = window.innerHeight;
+  larg = window.innerWidth;
 
-    function ajustaTela()  {
-        alt = window.innerHeight
-        larg = window.innerWidth
-        
-        console.log(alt, larg)
-    }
+  console.log(alt, larg);
+}
 
-    ajustaTela()
+ajustaTela();
 
-    function positionRandom() {
-        var posicaoX = Math.floor(Math.random() * larg) - 90
-        var posicaoY = Math.floor(Math.random() * alt) - 90
+function positionRandom() {
+  var posicaoX = Math.floor(Math.random() * larg) - 90;
+  var posicaoY = Math.floor(Math.random() * alt) - 90;
 
-        posicaoX = posicaoX < 0 ? 0 : posicaoX
-        posicaoY = posicaoY < 0 ? 0 : posicaoY
+  posicaoX = posicaoX < 0 ? 0 : posicaoX;
+  posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-        console.log(posicaoX, posicaoY)
+  console.log(posicaoX, posicaoY);
 
-        var mosquito = document.createElement('img')
-        mosquito.src = 'assets/imagens/mosca.png'
-        mosquito.className= tamanhoAleatorio() + ' ' + ladoAleatorio()
-        mosquito.style.left = posicaoX + 'px'
-        mosquito.style.top = posicaoY + 'px'
-        mosquito.style.position = 'absolute'
+  var mosquito = document.createElement("img");
+  mosquito.src = "assets/imagens/mosca.png";
+  mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
+  mosquito.style.left = posicaoX + "px";
+  mosquito.style.top = posicaoY + "px";
+  mosquito.style.position = "absolute";
 
-        document.body.appendChild(mosquito)
-        
-    }
-    
-    function tamanhoAleatorio() {
-        var classe = Math.floor(Math.random() * 3)
-        
-        switch(classe){
-            case 0:
-                return 'mosquito'  
-            
-            case 1:
-                return 'mosquito2'
+  document.body.appendChild(mosquito);
+}
 
-            case 2:
-                return 'mosquito3'
-        }
-    }
+function tamanhoAleatorio() {
+  var classe = Math.floor(Math.random() * 3);
 
-    function ladoAleatorio() {
-        var classe = Math.floor(Math.random() * 2)
-        
-        switch(classe){
-            case 0:
-                return 'ladoa'  
-            
-            case 1:
-                return 'ladob'
-        }
-    }
+  switch (classe) {
+    case 0:
+      return "mosquito";
+
+    case 1:
+      return "mosquito2";
+
+    case 2:
+      return "mosquito3";
+  }
+}
+
+function ladoAleatorio() {
+  var classe = Math.floor(Math.random() * 2);
+
+  switch (classe) {
+    case 0:
+      return "ladoa";
+
+    case 1:
+      return "ladob";
+  }
+}
